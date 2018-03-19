fa6257874ebaa95bd14af732a6066f7360767f10
two <1293460196@qq.com>
2018-03-19 15:15:59 +0800

    keep progress

--- 练习题/第8届C语言C组/7 progress/8_C_C_7.cpp
+++ 练习题/第8届C语言C组/7 progress/8_C_C_7.cpp
@@ -20,6 +20,16 @@
 	
 	1、我考虑的是先算出它有几个字母 （7种可能：初始：A AA AAA AAAA AAAAA  AAAAAA AAAAAAA） 
 	2、 然后根据情况，从末尾开始加数字，直到相同为止。同时字母要进行相应的变换 
+	以示例二为例子：
+	pow(26,2)+1  
+	AAA
+	然后就是不断加的过程：
+	首先是：
+	末尾Z
+
+	 
+	while(){
+	}
 */
 
 int main() {
@@ -43,21 +53,49 @@ int main() {
 		
 	}
 	else if(in <= pow(26,3)){
-		char result3[3] = {'Z','Z','Z'};
-		count = pow(26,3);
+		char result3[3] = {'A','A','A'};
+		/*
+	AAZ
+	ABA
+	ABZ
+	ACA
+	...
+	AZZ
+	BAA
+		*/
+		count = pow(26,2)+1;
 		i = 2;
 		printf("%d\n",in);
-		while(reult){
-			count = count -1;
-			result3[i] -= 1;
-			printf("%d\n",count);
-			if(count==in){
-				break;
+		while(1){
+			
+			//处理末位的变化 
+			if(i == 2&&result3[i]!='A'){
+				count = count+1;
+				result3[i] += 1;
+				printf("%d\n",count);
+				if(count==in){
+					break;
+				}
 			}
-			if(result3[i]=='Z'){
+			else if(i == 2&&result3[i]=='Z'){
+				result3[i] = 'A';
 				i--;
-				result3[i] = 'A'; 
+				result3[i] += 1; 
+			
 			}
+			//处理倒数第二位的变化
+			if(i == 1;&&result3[i+1]!='Z'&&result3[i]!='Z'){   //当末位不为Z，当前位不为Z时继续加 
+				count = count + 1;
+				reulst3[i+1]  += 1;
+				if(count==in){
+					break
+				}
+				if(i == 1&&result3[i+1]=='Z'&&result3[i]!='Z'){  //当末位为'Z' ,上一位不是'Z' 
+					result3[i+1] = 'A';
+					result3[i]  += 1;
+				} 
+			}
+		
 		}
 		for(i=0;i<=3;i++){
 			printf("%c",result3[i]);
---
+++ 练习题/第8届C语言C组/7 progress/others.cpp
@@ -0,0 +1,68 @@
+// 参考：http://blog.csdn.net/zhangge138/article/details/79590822 
+
+
+
+
+#include<stdio.h>  
+  
+char s[7] = {0};  
+  
+int main()  
+{  	
+	 char test = 'A';
+	 printf("%c",test-1); 
+     int x;  
+     
+     /*
+     	字母是可以相同的，就不用设置那么多判断了 
+	 	巧妙之处：
+		   关于如何设置前面几位是没有的时候，该怎么做。
+		    
+	 */
+     
+     scanf("%d",&x);  
+     for(int i = -1 ; i < 26 ; i++)  
+        for(int j = (i == -1)?-1 : 0 ; j < 26 ; j++)  
+             for(int k = (j == -1)?-1 : 0 ; k < 26 ; k++)  
+                 for(int l = (k == -1)?-1 : 0 ; l < 26 ; l++)  
+                      for(int m = (l == -1)?-1 : 0 ; m < 26 ; m++)  
+                          for(int n = (m == -1)?-1 : 0 ; n < 26 ; n++)  
+                                 for(int o = (n == -1)?-1 : 0 ; o < 26 ; o++)  
+                                   {  
+                                         s[0] = 'A' + i ;  
+                                         s[1] = 'A' + j ;  
+                                         s[2] = 'A' + k ;  
+                                         s[3] = 'A' + l ;  
+                                         s[4] = 'A' + m ;  
+                                         s[5] = 'A' + n ;  
+                                         s[6] = 'A' + o ;  
+                                         
+                                         /*
+											A表示第1列，
+											B表示第2列，
+											Z表示第26列，
+											AA表示第27列，
+											AB表示第28列，
+											BA表示第53列，
+										
+										*/
+                                         
+                                         int q = 0;  
+                                         for(int p = 0 ; p < 7 ; p ++)  
+                                         {  
+                                             q = q * 26 + (s[p] - 'A' + 1);  
+                                         }  
+                                         if(q == x)  
+                                         {  
+                                             for(int r = 0 ; r < 7 ; r++)  
+                                             {  
+                                                 if(s[r] == '@')printf("");  
+                                                  else printf("%c",s[r]);  
+                                             }  
+                                             printf("\n");  
+                                             return 0;  
+                                         }  
+                                   }  
+     return 0;  
+          
+}   
